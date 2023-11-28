ef7e96f3f038feae888d9b166bc5d6b8208e94d9
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2023-11-28 09:15:43 -0300

    Pronto

--- screens/Localization.jsx
+++ screens/Localization.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, ImageBackground, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { Magnetometer } from 'expo-sensors';
@@ -10,7 +10,7 @@ export default function Localization({ navigation }) {
   const [location, setLocation] = useState(null);
   const [compassData, setCompassData] = useState({ x: 0, y: 0, z: 0 });
   const [compassColor, setCompassColor] = useState('#000000');
-  const arrowSize = 40;
+  const arrowSize = 50;
   const [sentido, setsentido] = useState('Norte');
 
   useEffect(() => {
@@ -48,17 +48,17 @@ export default function Localization({ navigation }) {
     const angle = Math.atan2(data.y, data.x) * (180 / Math.PI) + 180;
 
     if (angle >= 45 && angle < 135) {
-      setCompassColor('#FF0000'); // Vermelho para leste
+      setCompassColor('#66EAA3'); // Vermelho para leste
       setsentido('Leste');
     } else if (angle >= 135 && angle < 225) {
-      setCompassColor('#00FF00'); // Verde para sul
-        setsentido('Sul');
+      setCompassColor('#A32350'); // Verde para sul
+      setsentido('Sul');
     } else if (angle >= 225 && angle < 315) {
-      setCompassColor('#0000FF'); // Azul para oeste
+      setCompassColor('#6750a4'); // Azul para oeste
       setsentido('Oeste');
     } else {
-      setCompassColor('#FFFF00'); // Amarelo para norte
-        setsentido('Norte');
+      setCompassColor('#50A3E8'); // Amarelo para norte
+      setsentido('Norte');
     }
   };
 
@@ -67,39 +67,39 @@ export default function Localization({ navigation }) {
       <Header title="Localização" />
       {location ? (
         <View>
-          <Text>Latitude: {location.latitude}</Text>
-          <Text>Longitude: {location.longitude}</Text>
-          <View style={{ height: 300, width: 300, alignSelf: 'center' }}>
-            <MapView
-              style={{ flex: 1 }}
-              initialRegion={{
-                latitude: location.latitude,
-                longitude: location.longitude,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+          <View style={{ alignItems: 'center', marginTop: 20 }}>
+            <Feather
+              name="arrow-up"
+              size={arrowSize}
+              color={compassColor}
+              style={{
+                transform: [{ rotate: `${compassData.x}deg` }],
               }}
-            >
-              <Marker
-                coordinate={{ latitude: location.latitude, longitude: location.longitude }}
-                title="Localização do dispositivo"
-                description="Esta é a atual localização do dispositivo."
-              />
-            </MapView>
+            />
+            <Text style={{ fontSize: 30, color: compassColor }}>{sentido}</Text>
           </View>
-          <View style={{ alignItems: 'center', marginTop: 20 }}>
-            <Text style={{ fontSize: 20, color: compassColor }}>Bússola</Text>
-          </View>
-          <View style={{ alignItems: 'center', marginTop: 20 }}>
-          <Feather
-            name="arrow-up"
-            size={arrowSize}
-            color={compassColor}
-            style={{
-              transform: [{ rotate: `${compassData.x}deg` }],
-            }}
-          />
-          <Text style={{ fontSize: 20, color: compassColor }}>{sentido}</Text>
-        </View>
+          <ImageBackground style={{ width: 350, height: 370, alignSelf: "center" }} source={require('../assets/bussola.png')}>
+  <View style={{ alignItems: 'center' }}>
+    <View style={{ height: 150, width: 150, alignSelf: 'center', marginTop: 110, overflow: 'hidden', borderRadius: 100, borderWidth: 2, borderColor: "black", marginRight: 3 }}>
+      <MapView
+        style={{ flex: 1, borderRadius: 60 }}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+          title="Localização do dispositivo"
+          description="Esta é a atual localização do dispositivo."
+        />
+      </MapView>
+    </View>
+  </View>
+</ImageBackground>
+
         </View>
       ) : (
         <Text>Carregando...</Text>
